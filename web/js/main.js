45bebb9968368fb9e9caa41d48d4223d11c85764
banan_developer <CleSStirSS@Gmail.com>
2026-09-21 22:11:52 +0300

    изменил ссылку перехода

--- web/js/main.js
+++ web/js/main.js
@@ -26,7 +26,7 @@ const App = {
                 })
                 const data = await res.json()
                 this.shortCode = data
-                this.shortLink = "http://127.0.0.1:8020/" + this.shortCode
+                this.shortLink = "http:/151.245.140:8020/" + this.shortCode
 
             }catch(err){
                 console.log(err)
